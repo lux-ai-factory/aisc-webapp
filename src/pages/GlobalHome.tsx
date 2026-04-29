d4768faf9f5e42f6874cd49fd9371499348a5231
valenti.canadell <valenti.canadell@uni.lu>
2026-04-30 00:56:19 +0200

    Update project list

--- src/pages/GlobalHome.tsx
+++ src/pages/GlobalHome.tsx
@@ -43,10 +43,16 @@ const ProjectsList = () => {
             <Typography component="h3" variant="h5" gutterBottom>
                 Projects
             </Typography>
-            <List>
+            {/*I would add more information to the project list, like creation date*/}
+            {/*Also I'm not a fan of leaving the empty left bar when on project list, to be thought about later*/}
+            <List className="project-list">
                 {projects.map((project) => (
                     <ListItem key={project.pid} disablePadding>
-                        <Link href={`/projects/${project.name}`} underline="hover">
+                        <Link
+                            href={`/projects/${project.name}`}
+                            underline="none"
+                            className="project-card"
+                        >
                             {project.name}
                         </Link>
                     </ListItem>
